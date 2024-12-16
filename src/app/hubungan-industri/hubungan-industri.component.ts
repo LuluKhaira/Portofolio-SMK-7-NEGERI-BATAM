18fcbf286dd71e41182bc048f29990066c0d2e7c
kontol <kemalalif57@gmail.com>
2024-12-16 09:19:47 +0700

    Alif

---
+++ src/app/hubungan-industri/hubungan-industri.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hubungan-industri',
+  standalone: true,
+  imports: [],
+  templateUrl: './hubungan-industri.component.html',
+  styleUrl: './hubungan-industri.component.css'
+})
+export class HubunganIndustriComponent {
+
+}
---
+++ src/app/program-kerja/program-kerja.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-program-kerja',
+  standalone: true,
+  imports: [],
+  templateUrl: './program-kerja.component.html',
+  styleUrl: './program-kerja.component.css'
+})
+export class ProgramKerjaComponent {
+
+}
