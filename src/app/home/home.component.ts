ebc4a61280568a4c52f83caec59baba934d0372a
kontol <kemalalif57@gmail.com>
2024-12-16 14:41:00 +0700

    alif

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,16 +1,21 @@
 import { Component, HostListener, signal } from '@angular/core';
 import { RouterModule } from '@angular/router';
-
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
   isScrolled = signal(false);
+  activeContent: string = 'identitas';
+
+  showContent(section: string): void {
+    this.activeContent = section;
+  }
 
   @HostListener('window:scroll', [])
   onWindowScroll() {
