373fb202d702673046eb71943db9444b1fa39387
kontol <kemalalif57@gmail.com>
2024-12-18 10:15:43 +0700

    alif

--- src/app/blog/blog.component.ts
+++ src/app/blog/blog.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
-
+import { Component, HostListener, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-blog',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './blog.component.html',
   styleUrl: './blog.component.css'
 })
 export class BlogComponent {
+  showFullDescription: boolean = false;
 
+  toggleDescription() {
+    this.showFullDescription = !this.showFullDescription;
+  }
+
+  isScrolled = signal(false);
+  activeContent: string = 'Semua';
+
+  constructor(private router: Router) { }
+
+  showContent(section: string): void {
+    this.activeContent = section;
+  }
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    this.isScrolled.set(window.scrollY > 50);
+  }
+
+  navigateToFullDescription() {
+    this.router.navigate(['/full-description']);
+  }
 }
